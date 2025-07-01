e72c96b1bae273bf793f15416da97b6ae2dbe529
firiusz <firiusz123@gmail.com>
2025-07-01 21:25:32 +0200

    added the resources , will work on the making them dependend on planet picked

--- scripts/control.gd
+++ scripts/control.gd
@@ -6,10 +6,26 @@ extends Control
 @onready var Planet_tab = $MenuPanel/HBoxContainer/TabContainer
 @onready var player = get_tree().get_root().get_node("Game/Game3D/Player")  # Adjust path to your player node
 
+@onready var Property_1 = $MenuPanel/HBoxContainer/TabContainer2/Resources/VBoxContainer/res1
+@onready var Property_2 = $MenuPanel/HBoxContainer/TabContainer2/Resources/VBoxContainer/res2
+@onready var Property_3 = $MenuPanel/HBoxContainer/TabContainer2/Resources/VBoxContainer/res3
+@onready var Property_4 = $MenuPanel/HBoxContainer/TabContainer2/Resources/VBoxContainer/res4
+
+
+
+#when the gui is ready
+
+
+
 #when the gui is ready
 func _ready():
 	Planet_tab.set_tab_title(0, "")
 	update_turn_label()
+	Property_1.text = ""
+	Property_2.text = ""
+	Property_3.text = ""
+	Property_4.text = ""
+	
 	# to be changed , dummy function for further pourpuses
 	
 func update_tab_name(new_name: String) -> void:
